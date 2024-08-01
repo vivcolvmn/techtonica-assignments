5939779dfadae977c7996e8375c5b7b9a5fa2585
vivcolvmn <vivcolvmn@gmail.com>
2024-08-01 13:20:35 -0700

    commit 25

---
+++ recipePage/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    windo.scrollTo(0, 0);
+    const ingredientsElement = document.querySelector(".ingredients");
+    const recipeElement = document.querySelector(".recipe");
+    const bottomElement = document.querySelector(".bottom");
+    const h1Element = document.querySelector("h1");
+    const topElement = document.querySelector(".top");
+    const photoElement = document.querySelector(".photo");
+
+    h1Element.innerHTML = "<img src='./resources.title.png'>";
+})
